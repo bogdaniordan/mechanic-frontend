ef12d0ca575ee14651b19b0f40a9106a072a84d9
bogdaniordan <bogdan.iordan47@yahoo.com>
2021-08-12 17:39:29 +0300

    Created sperata component for mechanic-custome chat

--- src/components/user/AppointmentsComponent.js
+++ src/components/user/AppointmentsComponent.js
@@ -7,7 +7,7 @@ import AuthServiceMechanic from "../../service/AuthServiceMechanic";
 import {InputLabel} from "@material-ui/core";
 import Select from "@material-ui/core/Select";
 import Modal from 'react-modal';
-import "./ChatModal.css"
+import ChatComponent from "../chatbot/ChatComponent";
 
 const customStyles = {
     content: {
@@ -57,7 +57,9 @@ const AppointmentsComponent = (props) => {
         } else {
             appointmentsByCustomerId();
         }
-    }, [appointments])
+    },
+        // otherwise it renders non stop
+        props.type ? [appointments] : [])
 
     const appointmentsByCustomerId = () => {
         AppointmentService.getByCustomerId(JSON.parse(localStorage.getItem("user")).customerId).then(res => {
@@ -156,116 +158,10 @@ const AppointmentsComponent = (props) => {
                                                             style={customStyles}
                                                             contentLabel="Example Modal"
                                                         >
-                                                            {/*<h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>*/}
                                                             <button onClick={closeModal}>close</button>
-                                                            <div className="container bootstrap snippets bootdeys">
-                                                                <div className="col-md-7 col-xs-12 col-md-offset-2">
-                                                                    <div className="panel" id="chat">
-                                                                        <div className="panel-heading">
-                                                                            <h3 className="panel-title">
-                                                                                <i className="icon wb-chat-text"
-                                                                                   aria-hidden="true"></i> Chat
-                                                                            </h3>
-                                                                        </div>
-                                                                        <div className="panel-body">
-                                                                            <div className="chats">
-                                                                                <div className="chat">
-                                                                                    <div className="chat-avatar">
-                                                                                        <a className="avatar avatar-online"
-                                                                                           data-toggle="tooltip"
-                                                                                           href="#"
-                                                                                           data-placement="right"
-                                                                                           title=""
-                                                                                           data-original-title="June Lane">
-                                                                                            <img
-                                                                                                src="https://bootdey.com/img/Content/avatar/avatar1.png"
-                                                                                                alt="..."/>
-                                                                                                <i></i>
-                                                                                        </a>
-                                                                                    </div>
-                                                                                    <div className="chat-body">
-                                                                                        <div className="chat-content">
-                                                                                            <p>
-                                                                                                Good morning, sir.
-                                                                                                <br/>What can I do for
-                                                                                                    you?
-                                                                                            </p>
-                                                                                            <time className="chat-time"
-                                                                                                  dateTime="2015-07-01T11:37">11:37:08
-                                                                                                am
-                                                                                            </time>
-                                                                                        </div>
-                                                                                    </div>
-                                                                                </div>
-                                                                                <div className="chat chat-left">
-                                                                                    <div className="chat-avatar">
-                                                                                        <a className="avatar avatar-online"
-                                                                                           data-toggle="tooltip"
-                                                                                           href="#"
-                                                                                           data-placement="left"
-                                                                                           title=""
-                                                                                           data-original-title="Edward Fletcher">
-                                                                                            <img
-                                                                                                src="https://bootdey.com/img/Content/avatar/avatar2.png"
-                                                                                                alt="..."/>
-                                                                                                <i></i>
-                                                                                        </a>
-                                                                                    </div>
-                                                                                    <div className="chat-body">
-                                                                                        <div className="chat-content">
-                                                                                            <p>Well, I am just looking
-                                                                                                around.</p>
-                                                                                            <time className="chat-time"
-                                                                                                  dateTime="2015-07-01T11:39">11:39:57
-                                                                                                am
-                                                                                            </time>
-                                                                                        </div>
-                                                                                    </div>
-                                                                                </div>
-                                                                                <div className="chat">
-                                                                                    <div className="chat-avatar">
-                                                                                        <a className="avatar avatar-online"
-                                                                                           data-toggle="tooltip"
-                                                                                           href="#"
-                                                                                           data-placement="right"
-                                                                                           title=""
-                                                                                           data-original-title="June Lane">
-                                                                                            <img
-                                                                                                src="https://bootdey.com/img/Content/avatar/avatar1.png"
-                                                                                                alt="..."/>
-                                                                                                <i></i>
-                                                                                        </a>
-                                                                                    </div>
-                                                                                    <div className="chat-body">
-                                                                                        <div className="chat-content">
-                                                                                            <p>
-                                                                                                If necessary, please ask
-                                                                                                me.
-                                                                                            </p>
-                                                                                            <time className="chat-time"
-                                                                                                  dateTime="2015-07-01T11:40">11:40:10
-                                                                                                am
-                                                                                            </time>
-                                                                                        </div>
-                                                                                    </div>
-                                                                                </div>
-                                                                            </div>
-                                                                        </div>
-                                                                        <div className="panel-footer">
-                                                                            <form>
-                                                                                <div className="input-group">
-                                                                                    <input type="text"
-                                                                                           className="form-control"
-                                                                                           placeholder="Say something"/>
-                                                                                      <span className="input-group-btn">
-                                                                                        <button className="btn btn-primary" type="button">Send</button>
-                                                                                      </span>
-                                                                                </div>
-                                                                            </form>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
+                                                            <ChatComponent messages={appointment.messages} />
+                                                            {/*<h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>*/}
+
                                                         </Modal>
 
                                                         {/*{!props.type ? (*/}
